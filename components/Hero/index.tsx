02f6246279947f015dacbbf28a5bebe68028b969
Brett Carruthers <brettcarruthers1@gmail.com>
2025-08-25 21:58:15 +1000

    xxx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -19,10 +19,7 @@ const Hero = () => {
                 🧠 Connected To Yourself
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
-                </span>
+                Unlock Your Potential with Connected To Yourself 
               </h1>
               <p>
                 Your journey to self-discovery starts here. Connected To Yourself
