243f9fa08a157f40710e81428c35023c3b32d447
Brett Carruthers <brettcarruthers1@gmail.com>
2025-08-25 21:37:52 +1000

    First changes

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -3,10 +3,10 @@ import BlogItem from "@/components/Blog/BlogItem";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blog Page - Solid SaaS Boilerplate",
+  title: "Blog Page - Connected To Yourself",
 
   // other metadata
-  description: "This is Blog page for Solid Pro"
+  description: "This is the Blog page for Connected To Yourself"
 };
 
 const BlogPage = async () => {
